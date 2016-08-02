4102660513c098f5c1ada41c6c3a6be067acdc4c
Peter <peter@kringles.org>
2016-08-01 21:03:02 -0700

    Broke out http access method for those not using https

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # pfsense_backup
 A shell script to monitor pfsense firewall changes and commit changes to a backup git repo.
 
+## requirements
+- bash
+- git
+- wget
+
 ## install
 - Create a private / secure git repo
 - Create a backup user on pfsense firewall that only access to diag_backup.php
--- backup_pf.sh
+++ backup_pf.sh
@@ -22,18 +22,18 @@ git pull $REPO master &> /dev/null
 
 # pulled these wget lines from pfsense's documentation
 wget -qO- --keep-session-cookies --save-cookies cookies.txt \
-  --no-check-certificate https://$HOSTNAME/diag_backup.php \
+  --no-check-certificate $HTTPACCESS://$HOSTNAME/diag_backup.php \
   | grep "name='__csrf_magic'" | sed 's/.*value="\(.*\)".*/\1/' > csrf.txt
 
 wget -qO- --keep-session-cookies --load-cookies cookies.txt \
   --save-cookies cookies.txt --no-check-certificate \
   --post-data "login=Login&usernamefld=$PFUSER&passwordfld=$PFPASSWD&__csrf_magic=$(cat csrf.txt)" \
-  https://$HOSTNAME/diag_backup.php  | grep "name='__csrf_magic'" \
+  $HTTPACCESS://$HOSTNAME/diag_backup.php  | grep "name='__csrf_magic'" \
   | sed 's/.*value="\(.*\)".*/\1/' > csrf2.txt
 
 wget -qO --keep-session-cookies --load-cookies cookies.txt --no-check-certificate \
   --post-data "Submit=download&donotbackuprrd=yes&__csrf_magic=$(head -n 1 csrf2.txt)" \
-  https://$HOSTNAME/diag_backup.php -O $DIRECTORY/${HOSTNAME}_config.xml
+  $HTTPACCESS://$HOSTNAME/diag_backup.php -O $DIRECTORY/${HOSTNAME}_config.xml
 
 # Look for differences and check them in if one is found
 git add ${HOSTNAME}_config.xml &> /dev/null
